2b54299bbe428ad8d10febcfa72a28abc5e68007
jlao96 <yingseng9608@gmail.com>
2025-05-25 17:27:53 -0600

    UnitIdentificationCodeErrors

--- ClassLibrary1/Rules/UnitIdentificationRules.cs
+++ ClassLibrary1/Rules/UnitIdentificationRules.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FluentResults;
 
 ///Esto debo arreglarlo
 namespace Dominio_Fermentación.Rules
