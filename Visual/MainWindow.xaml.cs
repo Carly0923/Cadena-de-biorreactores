64a6b502236f6a6f2cfc4ef7d5db1be6f8add2d4
DarkNHK <sgonzalezveriel@gmail.com>
2025-06-09 16:42:39 -0600

    Configuracion inicial de la ventana principal y creacion de las ventanas de gestion de lotes y de gestion de usuarios

--- Visual/MainWindow.xaml.cs
+++ Visual/MainWindow.xaml.cs
@@ -24,5 +24,68 @@ namespace Visual
         {
             InitializeComponent();
         }
+
+        /// Variable booleana para saber si hay un usuario autenticado
+        bool Autent = false;
+
+        ///Boton para abrir la ventana de gestión de usuarios
+        private void GU(object sender, RoutedEventArgs e)
+        {
+            if (Autent)
+            {
+                MessageBox.Show("Gestión de Usuarios");
+                GUsuarios gUsuarios = new GUsuarios();
+                this.Close();
+                gUsuarios.Show();
+            }
+
+            else
+            {
+                MessageBox.Show("Debes autenticarte");
+            }
+        }
+
+        /// Boton para abrir la pantalla de gestión de lotes
+        private void GL(object sender, RoutedEventArgs e)
+        {
+
+            if (Autent)
+            {
+                MessageBox.Show("Gestión de Lotes");
+                GLotes gLotes = new GLotes();
+                this.Hide();
+                gLotes.Show();
+
+            }
+
+            else
+            {
+                MessageBox.Show("Debes autenticarte");
+            }
+
+        }
+
+        /// Boton para iniciar y cerrar sesión
+        private void IS(object sender, RoutedEventArgs e)
+        {
+            if (Autent == false)
+            {
+
+                MessageBox.Show("Has iniciado sesión correctamente");
+                LoginButton.Content = "Cerrar Sesión";
+                Autent = true;
+
+            }
+
+            else
+            {
+                MessageBox.Show("Has cerrdo sesión correctamente");
+                LoginButton.Content = "Iniciar Sesión";
+                Autent = false;
+
+            }
+
+        }
+
     }
 }
